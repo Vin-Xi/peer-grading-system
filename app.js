5cd51cbae99d8a7ee4219a20825c62bc71105f90
Vin-Xi <hamdanrashid4@hotmail.com>
2021-01-05 16:24:38 +0500

    Dir structure fix

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const express=require("express"),
     Course=require("./models/course")
     Enroll=require("./models/enrollcourse")
     Assignment=require("./models/assignment")
-    //const uri = "mongodb+srv://project1:project1@cluster0.ilcak.mongodb.net/project?retryWrites=true&w=majority";
-    const uri = "mongodb+srv://user1:user123@cluster0.wm8lw.mongodb.net/peer-grading-system?retryWrites=true&w=majority"; //maira's db
-
+    const uri = "mongodb+srv://project1:project1@cluster0.ilcak.mongodb.net/project?retryWrites=true&w=majority";
+    //const uri = "mongodb+srv://hani:uhmi10149658@cluster0.4bvup.mongodb.net/<dbname>?retryWrites=true&w=majority";    //Hani's db
+    //const uri = "mongodb+srv://user1:user123@cluster0.wm8lw.mongodb.net/peer-grading-system?retryWrites=true&w=majority"; //maira's db
+    //const uri = " mongodb+srv://maha:maharana@cluster0.x89gb.mongodb.net/peerGrading?retryWrites=true&w=majority"; //Maha's db
 
 //-------------------------Database Configuration-----------------------
 mongoose.set('useNewUrlParser',true)
